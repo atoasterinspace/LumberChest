cd37ed3255dfdbbb51b4336a29b65881f1813679
shandriy <entyspite@gmail.com>
2024-04-30 16:58:43 -0700

    Added canvas reset JS

--- web/main.js
+++ web/main.js
@@ -7,10 +7,20 @@ Konstantin Edunov - 2024
 */
 
 window.addEventListener("DOMContentLoaded", function() {
-  const textureCanvas = document.getElementsByTagName("canvas")[0];
   const normalCanvas = document.getElementsByTagName("canvas")[1];
+  const textureCanvas = document.getElementsByTagName("canvas")[0];
   const specularCanvas = document.getElementsByTagName("canvas")[2];
-  const textureContext = textureCanvas.getContext("2d", { alpha: true });
   const normalContext = normalCanvas.getContext("2d", { alpha: true });
+  const textureContext = textureCanvas.getContext("2d", { alpha: true });
   const specularContext = specularCanvas.getContext("2d", { alpha: true });
+  normalCanvas.setAttribute("width", "1024");
+  normalCanvas.setAttribute("height", "1024");
+  textureCanvas.setAttribute("width", "1024");
+  textureCanvas.setAttribute("height", "1024");
+  specularCanvas.setAttribute("width", "1024");
+  specularCanvas.setAttribute("height", "1024");
+  normalContext.clearRect(0, 0, 1024, 1024);
+  textureContext.clearRect(0, 0, 1024, 1024);
+  specularContext.clearRect(0, 0, 1024, 1024);
+  
 });
